840f7a5649a955ff5247fbf822a8a4bff49f92e7
Sergey Poluyan <svpoluyan@gmail.com>
2018-04-26 00:09:26 +0300

    pdf

---
+++ mvquantilefun.r
@@ -0,0 +1,25 @@
+set.seed(1)
+n = 100
+x = seq(-10,10,length=n)
+y = seq(-10,10,length=n)
+
+#x
+
+# exponential centers
+centers = NULL
+centers = rbind (centers,  cbind (3, 3))
+centers = rbind (centers,  cbind (-5, 0))
+centers = rbind (centers,  cbind (0, -5))
+
+# PDF function
+threeExp = function (x,y)
+{
+  rez <- 0
+  rez <- rez + exp(-((x - centers[1,1])^2 + (y - centers[1,2])^2)*0.75)
+  rez <- rez + exp(-((x - centers[2,1])^2 + (y - centers[2,2])^2)*0.5)*0.75
+  rez <- rez + exp(-((x - centers[3,1])^2 + (y - centers[3,2])^2)*0.25)*0.5
+  rez
+}
+
+# PDF
+persp( outer( x, y, Vectorize(threeExp) ) )
