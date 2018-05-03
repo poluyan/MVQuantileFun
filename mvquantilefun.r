a96783cfa70508f4641edd4a15766dbc886980b1
Sergey Poluyan <svpoluyan@gmail.com>
2018-05-03 21:49:15 +0300

    col sum and row cumsums

--- mvquantilefun.r
+++ mvquantilefun.r
@@ -35,3 +35,31 @@ for(i in 1:n)
   cdf[i,] <- cumsum(pdf[i,])
 
 col <- cdf[,ncol(cdf)]
+
+s = sum(col)
+for(i in 1:n)
+  col[i] = col[i]/s
+
+col <- c(0, cumsum(col))
+
+#write.csv(col)
+
+row <- matrix(nrow=n, ncol=n + 1) 
+for(i in 1:n)
+{
+  t <- pdf[i,]
+
+  plot(t)
+
+  s <- sum(t)
+  for(j in 1:n)
+    t[j] <- t[j]/s
+
+  t <- cumsum(t)
+
+  plot(t)
+
+  row[i,1] <- 0
+  for(j in 2:(n+1))
+    row[i,j] <- t[j-1]
+}
