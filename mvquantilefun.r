86aeed680f3eba82aa93306feda5d4129febe4aa
Sergey Poluyan <svpoluyan@gmail.com>
2018-05-04 19:51:02 +0300

    making linear interpolation

--- mvquantilefun.r
+++ mvquantilefun.r
@@ -49,7 +49,7 @@ for(i in 1:n)
 {
   t <- pdf[i,]
 
-  plot(t)
+  #plot(t)
 
   s <- sum(t)
   for(j in 1:n)
@@ -57,9 +57,39 @@ for(i in 1:n)
 
   t <- cumsum(t)
 
-  plot(t)
+  #plot(t)
 
   row[i,1] <- 0
   for(j in 2:(n+1))
     row[i,j] <- t[j-1]
 }
+
+
+n = 1000 # number of sample
+random = NULL
+sample = NULL
+for(i in 1:n)
+{
+  r1 <- runif(1, 0, 1)
+  r2 <- runif(1, 0, 1)
+
+  # linear interpolation
+  index1 <- which(col == min(col[col>r1])) - 1
+  x0 <- x[index1]
+  y0 <- col[index1]
+  x1 <- x[index1 + 1]
+  y1 <- col[index1 + 1];
+  fx <- x0 + (r1 - y0) * (x1 - x0) / (y1 - y0);
+
+  index2 <- which(row[index1,] == min(row[index1,][row[index1,]>r2])) - 1
+  x0 <- y[index2]
+  y0 <- row[index1,index2]
+  x1 <- y[index2 + 1]
+  y1 <- row[index1,index2 + 1]
+  fy <- x0 + (r2 - y0) * (x1 - x0) / (y1 - y0)
+	
+  random = rbind (random,  cbind (r1, r2))
+  sample = rbind (sample,  cbind (fx, fy))
+}
+plot(random)
+plot(sample)
